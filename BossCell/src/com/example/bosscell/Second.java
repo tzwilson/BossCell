fdea015abb0f4303ddce1cfe04f6371e57e94458
tzwilson <tzwilson@mavs.coloradmesa.edu>
2015-03-12 04:05:07 -0600

    First Commit

---
+++ BossCell/src/com/example/bosscell/Second.java
@@ -0,0 +1,16 @@
+package com.example.bosscell;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    //this.setContentView(com.example.bosscell);
+	    // TODO Auto-generated method stub
+	}
+
+}
